264886aa946f3e43fecd3c352bc5f655c1f9b9ae
Orlando Javier <oj.flores118@gmail.com>
2024-08-21 17:14:48 -0600

    Dark mode en index

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     eventListeners();
+    darkMode();
 });
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu')
     mobileMenu.addEventListener('click', navegacionResponsive)
 };
 
+function darkMode() {
+    const botonDarkMode = document.querySelector('.dark-mode-boton')
+    botonDarkMode.addEventListener('click', classDarkMode)
+}
+
+function classDarkMode () {
+    document.body.classList.toggle('dark-mode');
+}
 function navegacionResponsive() {
     const navegacion = document.querySelector('.navegacion');
 
