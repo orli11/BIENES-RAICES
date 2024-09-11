1d67a4b6faa3440a51c2d717fa46434e1caaf3d3
Orlando Javier <oj.flores118@gmail.com>
2024-09-11 10:10:50 -0600

    Paginas estaticas completas

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,21 @@ function eventListeners() {
 };
 
 function darkMode() {
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    //console.log(prefiereDarkMode)
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+        
+    prefiereDarkMode.addEventListener('change', function() {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
     const botonDarkMode = document.querySelector('.dark-mode-boton')
     botonDarkMode.addEventListener('click', classDarkMode)
 }
